89eb39529d3273000e85e6006551d3a7e9b39a26
Tanishq Sharma <99052355+t7phy@users.noreply.github.com>
2024-03-23 14:16:00 +0000

    towards sidis

---
+++ eh/unpol_coeff.py
@@ -0,0 +1,113 @@
+from core.definitions import CA, CF, ZETA2, ZETA3
+from numpy import log
+
+def ct_nlo_qq_rr(x, z):
+    result = 2 * CF
+    return result
+
+def ct_nlo_qq_rs(x, z):
+    result = 2 * CF
+    return result
+
+def ct_nlo_qq_rl(x, z):
+    result = 2 * CF
+    return result
+
+def ct_nlo_qq_sr(x, z):
+    result = 2 * CF
+    return result
+
+def ct_nlo_qq_ss(x, z):
+    result = 2 * CF
+    return result
+
+def ct_nlo_qq_sl(x, z):
+    result = 2 * CF
+    return result
+
+def ct_nlo_qq_lr(x, z):
+    result = 2 * CF
+    return result
+
+def ct_nlo_qq_ls(x, z):
+    result = 2 * CF
+    return result
+
+def ct_nlo_qq_ll(x, z):
+    result = 2 * CF
+    return result
+
+def ct_nlo_gq_rr(x, z):
+    result = 2 * CF
+    return result
+
+def ct_nlo_gq_rs(x, z):
+    result = 2 * CF
+    return result
+
+def ct_nlo_gq_rl(x, z):
+    result = 2 * CF
+    return result
+
+def ct_nlo_gq_sr(x, z):
+    result = 2 * CF
+    return result
+
+def ct_nlo_gq_ss(x, z):
+    result = 2 * CF
+    return result
+
+def ct_nlo_gq_sl(x, z):
+    result = 2 * CF
+    return result
+
+def ct_nlo_gq_lr(x, z):
+    result = 2 * CF
+    return result
+
+def ct_nlo_gq_ls(x, z):
+    result = 2 * CF
+    return result
+
+def ct_nlo_gq_ll(x, z):
+    result = 2 * CF
+    return result
+
+def ct_nlo_qg_rr(x, z):
+    return
+
+def ct_nlo_qg_rs(x, z):
+    return
+
+def ct_nlo_qg_rl(x, z):
+    return
+
+def ct_nlo_qg_sr(x, z):
+    return
+
+def ct_nlo_qg_ss(x, z):
+    return
+
+def ct_nlo_qg_sl(x, z):
+    return
+
+def ct_nlo_qg_lr(x, z):
+    return
+
+def ct_nlo_qg_ls(x, z):
+    return
+
+def ct_nlo_qg_ll(x, z):
+    return
+
+def cl_nlo_qq_rr(x, z):
+    result = 8 * CF * x * z
+    return result
+
+def cl_nnlo_gq_rr(x, z):
+    result = 8 * CF * x * (1 - z)
+    return result
+
+def cl_nlo_qg_rr(x, z):
+    result = 8 * x * (1 - x)
+    return result
